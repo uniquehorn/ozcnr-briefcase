ebfe98018b9e7e1c9150687fb981db6c8523762f
Caner Özer <canerozer44@hotmail.com>
2020-10-22 16:35:32 +0300

    Yerel klasör tamamlandı

---
+++ EvrakCantam/EvrakCantam.cpp
@@ -0,0 +1,69 @@
+﻿// EvrakCantam.cpp : Defines the entry point for the application.
+//
+
+#include "EvrakCantam.h"
+#include <filesystem>
+#include <ctime>
+#include <chrono>
+
+namespace fs = std::filesystem;
+
+using namespace std;
+
+// Settings
+const string AYARDOSYA = "";
+
+string yerelYol = "C:\\Users\\Caner\\Desktop\\testDir1\\";
+string seyyarYol = "C:\\Users\\Caner\\Desktop\\testDir2\\";
+
+struct dosya {
+	string yol;
+	uintmax_t boyut;
+	time_t tarih;
+};
+
+vector<dosya> yerelDosyalar;
+
+template <typename TP>
+std::time_t to_time_t(TP tp)
+{
+	using namespace std::chrono;
+	auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now()
+		+ system_clock::now());
+	return system_clock::to_time_t(sctp);
+}
+
+int main()
+{
+	ayarlariYukle();
+	cout << yerelYol << endl;
+	cout << seyyarYol << endl << endl;
+
+	yerelKlasorOku();
+
+	return 0;
+}
+
+//Load settings from settings file.
+bool ayarlariYukle()
+{
+	return true;
+}
+
+//Scan local files
+void yerelKlasorOku()
+{
+	int i = 0;
+	for (const auto& rawYerelDosyalar : fs::recursive_directory_iterator(yerelYol))
+	{
+		yerelDosyalar.push_back(dosya());
+		yerelDosyalar[i].yol = rawYerelDosyalar.path().string();
+		yerelDosyalar[i].boyut = fs::file_size(rawYerelDosyalar.path());
+		yerelDosyalar[i].tarih = to_time_t(fs::last_write_time(rawYerelDosyalar.path()));
+
+		char buff[40];
+		strftime(buff, 40, "%d.%m.%Y", localtime(&yerelDosyalar[i].tarih));
+		std::cout << yerelDosyalar[i].yol << "\t" << yerelDosyalar[i].boyut << "\t" << buff << std::endl;
+	}
+
+}
---
+++ EvrakCantam/EvrakCantam.h
@@ -0,0 +1,12 @@
+﻿// EvrakCantam.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// Interface
+bool ayarlariYukle();
+void yerelKlasorOku();
+
+// TODO: Reference additional headers your program requires here.
